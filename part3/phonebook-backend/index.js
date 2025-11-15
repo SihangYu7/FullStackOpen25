9b399d777bbac693c28fc68daf4b7de1bce440e5
Sihang <sefton4eva@gmail.com>
2025-11-15 19:06:47 +0200

    update exercise till 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -42,6 +42,7 @@ morgan.token("body", (request) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 app.get("/api/persons", (request, response) => {
